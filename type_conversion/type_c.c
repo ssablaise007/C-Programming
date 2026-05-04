353f345f162cca11f7deeb914feeac449b2af222
ssablaise007 <ssablaise007@gmail.com>
2026-05-05 00:45:29 +0530

    Added multiple folders

---
+++ type_conversion/type_c.c
@@ -0,0 +1,25 @@
+/*
+ * type_c.c
+ *
+ *  Created on: 04-May-2026
+ *      Author: Shoyab Silar
+ */
+
+
+#include<stdio.h>  // Preprocessor Section
+
+int main() // Main Function
+{
+	int x = 66;
+	char p='p';
+	float y=73.8;
+	int z = -55;
+
+	printf("\n Integer and character value of x :%d --> %C",x,x);
+	printf("\n Integer and character value of p :%d --> %C",p,p);
+	printf("\n Integer and Hex value of x :%d --> %x",x,x);
+	printf("\n Integer and octal value of x :%f --> %o",x,x);
+	printf("\n Integer and float value of y :%f --> %d",y,y);
+	printf("\n Integer and unsigned value of z :%d --> %u",z,z);
+
+}
