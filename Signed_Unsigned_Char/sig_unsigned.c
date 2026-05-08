a3dc283089296f93ef877df0f2fbd9258e99c7da
ssablaise007 <ssablaise007@gmail.com>
2026-05-08 20:03:16 +0530

    Learning

---
+++ Signed_Unsigned_Char/sig_unsigned.c
@@ -0,0 +1,24 @@
+/*
+ * sig_unsigned.c
+ *
+ *  Created on: 08-May-2026
+ *      Author: Shoyab Silar
+ */
+
+
+#include<stdio.h>  // Preprocessor Section
+
+int main() // Main Function
+{
+	// un-signed Char Range is : 0 to 255
+	unsigned char  a=260;
+	unsigned char  b=-3;
+
+	// Signed Char Range is : -128 to 127
+	signed char x=10;
+	signed char y=20;
+
+	printf("Value of A is : %d" , a);
+	printf(" \nValue of B is : %d" , b);
+
+}
