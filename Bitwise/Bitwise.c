f46d2001b0d7c589d8b660e3c180e0db1b2f6640
ssablaise007 <ssablaise007@gmail.com>
2026-05-09 00:25:56 +0530

    Learning

---
+++ Bitwise/Bitwise.c
@@ -0,0 +1,31 @@
+/*
+ * Bitwise.c
+ *
+ *  Created on: 09-May-2026
+ *      Author: Shoyab Silar
+ */
+
+#include<stdio.h>  // Preprocessor Section
+
+int main() // Main Function
+{
+	int a = 10;
+	//int b = 15;
+	//int c = 122;
+
+	printf("Value of a currently is : %d\n", a);
+
+	a += 10;
+	printf("Value of a is %d\n", a);
+
+	a -= 10;
+		printf("Value of a is %d\n", a);
+
+	a *= 10;
+		printf("Value of a is %d\n", a);
+
+	a /= 10;
+		printf("Value of a is %d\n", a);
+	return 0;
+}
+
